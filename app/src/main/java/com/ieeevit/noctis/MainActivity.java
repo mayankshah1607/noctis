f03c167a4572c68b01a0d849914fafec7b111d9e
mayankshah1607 <mayankshah1614@gmail.com>
2018-02-25 20:57:28 +0530

    Testing

--- app/src/main/java/com/ieeevit/noctis/MainActivity.java
+++ app/src/main/java/com/ieeevit/noctis/MainActivity.java
@@ -1,13 +1,47 @@
 package com.ieeevit.noctis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login,register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        open();
+
     }
+    public void open(){
+        login = (Button) findViewById(R.id.button_1);
+        register = (Button) findViewById(R.id.button_2);
+
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent lgn = new Intent("com.ieeevit.noctis.LoginPage");
+                        startActivity(lgn);
+                    }
+                }
+        );
+
+        register.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent reg = new Intent("com.ieeevit.noctis.RegisterPage");
+                        startActivity(reg);
+                    }
+                }
+        );
+
+    }
+
+
+
+
 }
