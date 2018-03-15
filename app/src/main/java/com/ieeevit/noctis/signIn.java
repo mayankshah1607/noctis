0c09d1cd6a61ade5545d4df1d30a4c93cc840338
mayankshah1607 <mayankshah1614@gmail.com>
2018-03-15 22:21:15 +0530

    Error in main login page

--- app/src/main/java/com/ieeevit/noctis/signIn.java
+++ app/src/main/java/com/ieeevit/noctis/signIn.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class signIn extends AppCompatActivity {
     Button signup;
     @Override
