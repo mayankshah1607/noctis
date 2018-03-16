5d268206c51d88af69cad95cc309711ef14e2c76
mayankshah1607 <mayankshah1614@gmail.com>
2018-03-16 22:35:06 +0530

    Updated Sign In

--- app/src/main/java/com/ieeevit/noctis/passReset.java
+++ app/src/main/java/com/ieeevit/noctis/passReset.java
@@ -10,4 +10,10 @@ public class passReset extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_reset);
     }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+    }
 }
--- app/src/main/java/com/ieeevit/noctis/signIn.java
+++ app/src/main/java/com/ieeevit/noctis/signIn.java
@@ -1,13 +1,21 @@
 package com.ieeevit.noctis;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
@@ -15,15 +23,61 @@ public class signIn extends AppCompatActivity {
     Button signup,login;
     TextView forgotPass;
     EditText email,pass;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        login = (Button) findViewById(R.id.loginbtn);
+        email = (EditText) findViewById(R.id.emailEditText);
+        pass = (EditText) findViewById(R.id.passEditText);
         open();
-        loginApp();
         resetPass();
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startApp();
+                    }
+                }
+        );
+
     }
+
+    private void startApp() {
+        mAuth = FirebaseAuth.getInstance();
+        String Email= email.getText().toString().trim();
+        String Pass = pass.getText().toString().trim();
+        login.setText("Logging in...");
+        if (Email.isEmpty()) {
+            login.setText("Log in");
+            Toast.makeText(getApplicationContext(),"Please enter your Email ID", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (Pass.isEmpty()) {
+            login.setText("Log in");
+            Toast.makeText(getApplicationContext(),"Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(Email,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                login.setText("Log in");
+                if (task.isSuccessful()){
+                    //Sign In
+                    Intent mainIntent = new Intent(signIn.this,Main.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Please check your credentials and try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
 
     private void resetPass() {
         forgotPass = (TextView) findViewById(R.id.forgot);
@@ -39,17 +93,7 @@ public class signIn extends AppCompatActivity {
         );
     }
 
-    private void loginApp() {
-        login = (Button) findViewById(R.id.loginbtn);
-        login.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        //Sign in
-                    }
-                }
-        );
-    }
+
 
     public void open(){
         signup = (Button) findViewById(R.id.signupbtn);
